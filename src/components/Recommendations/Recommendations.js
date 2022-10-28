996a967185b442ea6eb7fdce2e75dc2f3783e018
b0p1 <sofiarojo09@gmail.com>
2022-10-28 14:46:10 -0400

    basic styles done for cards

--- src/components/Recommendations/Recommendations.js
+++ src/components/Recommendations/Recommendations.js
@@ -1,28 +1,36 @@
 import "./recommendations.scss";
 import RecommendationsCard from "../RecommendationsCard/RecommendationsCard";
 import sandboxLogo from "../../assets/images/sandbox-logo.svg";
+import Card2 from "../RecommendationsCard/Card2";
+import Card3 from "../RecommendationsCard/Card3";
 
 function Recommendations(props) {
   return (
     // "recom" short for recommendation
     <div className="recom-all">
-       <div className="sandbox-header">
-            <img className="header-logo" src={sandboxLogo} />
-        </div>
+      <div className="sandbox-header">
+        <img className="header-logo" src={sandboxLogo} />
+      </div>
       <div className="recom-top">
         <div className="recom-top__title">
           <h1 className="recom-top-main__title">Awesome!</h1>
-          <h2 className="recom-top__subtitle">Your profile has been successfully created.</h2>
+          <h2 className="recom-top__subtitle">
+            Your profile has been successfully created.
+          </h2>
         </div>
       </div>
       <div>
-        <h3 className="recom-top__subtitles">Now let’s start making some friends...</h3>
-        <h3 className="recom-top__subtitles">Here are some of your colleagues with common interests.</h3>
+        <h3 className="recom-top__subtitles">
+          Now let’s start making some friends...
+        </h3>
+        <h3 className="recom-top__subtitles">
+          Here are some of your colleagues with common interests.
+        </h3>
       </div>
       <div className="recom-bottom">
         <RecommendationsCard />
-        <RecommendationsCard />
-        <RecommendationsCard />
+        <Card2 />
+        <Card3 />
       </div>
     </div>
   );
