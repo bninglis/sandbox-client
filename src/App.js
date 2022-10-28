629cf49a94d5cae4e7e3044c23bb77c88e819c7f
bninglis <107963185+bninglis@users.noreply.github.com>
2022-10-28 10:22:42 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignupPage from "./components/SignupPage/SignupPage";
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SignupPage />} />
-          <Route path="/background-info" element={<Background />} />
-          {/* <Route path="/interests" element={<Interests />} /> */}
-          {/* <Route path="/interests" element={<Interests />} /> */}
-          <Route path="/recommendations" element={<Recommendations />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<SignupPage />} />
+                    <Route path="/background-info" element={<Background />} />
+                    {/* <Route path="/interests" element={<Interests />} /> */}
+                    {/* <Route path="/interests" element={<Interests />} /> */}
+                    <Route path="/recommendations" element={<Recommendations />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
