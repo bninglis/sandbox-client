d2d0e6e28f80a19872348612aba66fb54e1de86a
bninglis <107963185+bninglis@users.noreply.github.com>
2022-10-27 22:35:03 -0700

    finishes background

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,11 +1,59 @@
 import FormProgress from "../FormSlider/FormProgress";
+import { useState } from "react";
 
 export default function Background() {
+    const [isFormValid, setIsFormValid] = useState();
+    const company = sessionStorage.getItem("company");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const handlerFormData = { role: e.target.role.value, department: e.target.department.value, tenure: e.target.tenure.value };
+        if (!handlerFormData.role || !handlerFormData.department || !handlerFormData.tenure) {
+            setIsFormValid(false);
+        } else {
+            setIsFormValid(true);
+            sessionStorage.setItem("role", handlerFormData.role);
+            sessionStorage.setItem("department", handlerFormData.department);
+            sessionStorage.setItem("tenure", handlerFormData.tenure);
+        }
+    };
     return (
         <>
-            <h1>Background</h1>
-            <form>
-                <input className="create__field" type="text" name="firstname" id="firstname" placeholder="First Name" />
+            <h1 className="followup__title">Background</h1>
+            <form className="background" onSubmit={handleSubmit}>
+                <div className="background__input">
+                    <div className="background__fields">
+                        <label className="followup__label" htmlFor="role">
+                            What is your role within the company?
+                        </label>
+                        <input className="background__field" type="text" name="role" id="role" />
+                        <label className="followup__label" htmlFor="department">
+                            What department do you work in?
+                        </label>
+                        <input className="background__field" type="text" name="department" id="department" />
+                    </div>
+                    <div className="background__tenure">
+                        <p className="followup__text">How long have you been with {company}?</p>
+                        <input className="background__radio" type="radio" name="tenure" value="lessthanone" />
+                        <label className="followup__label followup__label--radio" htmlFor="lessthan1">
+                            Less than 1 year
+                        </label>
+                        <input className="background__radio" type="radio" name="tenure" value="1to2" />
+                        <label className="followup__label followup__label--radio" htmlFor="1to2">
+                            1-2 years
+                        </label>
+                        <input className="background__radio" type="radio" name="tenure" value="3to5" />
+                        <label className="followup__label followup__label--radio" htmlFor="3to5">
+                            3-5 years
+                        </label>
+                        <input className="background__radio" type="radio" name="tenure" value="5plus" />
+                        <label className="followup__label followup__label--radio" htmlFor="5plus">
+                            5+ years
+                        </label>
+                    </div>
+                </div>
+                <button className="background__submit" type="submit">
+                    Next
+                </button>
             </form>
         </>
     );
