15d59e1507a05638cf46da54a812379f4bf630ab
bninglis <107963185+bninglis@users.noreply.github.com>
2022-10-27 21:32:16 -0700

    sets up formprogress

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,7 +1,12 @@
 import FormProgress from "../FormSlider/FormProgress";
 
 export default function Background() {
-    return <>
-        <FormProgress />
-    </>;
+    return (
+        <>
+            <h1>Background</h1>
+            <form>
+                <input className="create__field" type="text" name="firstname" id="firstname" placeholder="First Name" />
+            </form>
+        </>
+    );
 }
--- src/components/FormSlider/FormProgress.js
+++ src/components/FormSlider/FormProgress.js
@@ -3,18 +3,10 @@ import ReactSlider from "react-slider";
 import { useState } from "react";
 
 export default function FormProgress() {
-    const [progress, setProgress] = useState(0)
+    const [progress, setProgress] = useState(0);
     return (
-        <>
-            <ReactSlider className="progress"
-                thumbClassName="progress__thumb"
-                trackClassName="progress__track"
-                markClassName="progress__mark"
-                min="0"
-                max="99"
-                value={progress}
-                marks="33"
-                />
-        </>
+        <div className="progress__container">
+            <ReactSlider className="progress" thumbClassName="progress__thumb" trackClassName="progress__track" markClassName="progress__mark" min={0} max={100} value={progress} marks={50} disabled />
+        </div>
     );
 }
